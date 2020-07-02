8db3cd78680f0d235aaf4871bcd7c314b700cb57
liuzhilin <764166218@qq.com>
2020-07-02 10:31:16 +0800

    增加本地文件读取和后期增加配置

--- src/main/java/org/github/process/ProcessBootStrap.java
+++ src/main/java/org/github/process/ProcessBootStrap.java
@@ -1,17 +1,20 @@
 package org.github.process;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.github.process.config.Configuration;
 import org.github.process.exception.ProcessException;
 import org.github.process.thread.DefaultFactory;
 import org.github.process.tool.abs.AbstractProcess;
 import org.github.process.tool.impl.ShellProcess;
 import org.github.process.util.ShellThreadPool;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.*;
-
-import static org.github.process.config.Configuration.*;
 
 
 /**
@@ -27,9 +30,15 @@ public class ProcessBootStrap {
      */
     private final LinkedBlockingQueue<AbstractProcess> queue = new LinkedBlockingQueue<AbstractProcess>();
 
+    /**
+     * 流程个数
+     */
     private Integer processSize = 0;
 
-    private Properties properties ;
+    /**
+     * 整体配置项
+     */
+    private Properties properties;
 
     /**
      * 执行线程池
@@ -42,7 +51,29 @@ public class ProcessBootStrap {
     /**
      * Shell线程池
      */
-    private final ShellThreadPool shellThreadPool = new ShellThreadPool(FTP_HOST,FTP_PORT,FTP_USERNAME,FTP_PASSWORD);
+    private ShellThreadPool shellThreadPool;
+
+    public ProcessBootStrap() {
+        init();
+    }
+
+    private void init()  {
+        //初始化配置
+        this.properties = new Properties();
+        try {
+            properties.load(new FileInputStream(new File("src\\main\\resources\\process.properties")));
+        }catch (Exception e){
+            log.warn("读取本地配置失败");
+        }
+        //初始化shell
+        if (this.properties.containsKey("ssh.host")) {
+            String host = this.properties.getProperty("ssh.host");
+            int port = Integer.valueOf(this.properties.getProperty("ssh.port"));
+            String username = this.properties.getProperty("ssh.username");
+            String password = this.properties.getProperty("ssh.password");
+            this.shellThreadPool = new ShellThreadPool(host, port, username, password);
+        }
+    }
 
     /**
      * 添加流程
@@ -57,14 +88,24 @@ public class ProcessBootStrap {
 
     /**
      * 添加配置
+     *
      * @param properties
      * @return
      */
-    public ProcessBootStrap setConfig(Properties properties){
+    public ProcessBootStrap setConfig(Properties properties) {
         this.properties = properties;
         return this;
     }
 
+    public ProcessBootStrap setConfig(String key,String value) {
+        this.properties.setProperty(key, value);
+        return this;
+    }
+
+    public ProcessBootStrap setConfig(Map<String,String> map) {
+        this.properties.putAll(map);
+        return this;
+    }
 
 
     /**
@@ -89,10 +130,11 @@ public class ProcessBootStrap {
     /**
      * 内置操作类
      * 内定了一些操作
+     *
      * @return
      */
-    public Operation stream(){
-        return new Operation(queue,this);
+    public Operation stream() {
+        return new Operation(queue, this);
     }
 
     /**
@@ -100,34 +142,40 @@ public class ProcessBootStrap {
      */
     public void shutdown() {
         singleThreadExecutor.shutdown();
-        shellThreadPool.close();
+        if (shellThreadPool != null) {
+            shellThreadPool.close();
+        }
     }
 
     /**
      * 内定操作类，定义了一些默认操作
-     *
      */
-    public class Operation{
+    public class Operation {
         private LinkedBlockingQueue<AbstractProcess> queue;
         private ProcessBootStrap processBootStrap;
 
-        public Operation(LinkedBlockingQueue<AbstractProcess> queue,ProcessBootStrap processBootStrap) {
+        public Operation(LinkedBlockingQueue<AbstractProcess> queue, ProcessBootStrap processBootStrap) {
             this.queue = queue;
             this.processBootStrap = processBootStrap;
         }
 
         /**
          * 上传文件
-         * @param sourcePath 文件路径
+         *
+         * @param sourcePath     文件路径
          * @param sourceFileName 文件名
-         * @param targetPath 目标路径
+         * @param targetPath     目标路径
          * @param targetFileName 目标文件名
          * @return
          */
-        public Operation upLoadFile(String sourcePath,String sourceFileName,String targetPath,String targetFileName){
+        public Operation upLoadFile(String sourcePath, String sourceFileName, String targetPath, String targetFileName) {
             AbstractProcess process = new AbstractProcess() {
                 @Override
                 public boolean invoke() throws Exception {
+                    if (shellThreadPool == null) {
+                        log.info("请配置ssh地址");
+                        return false;
+                    }
                     shellThreadPool.uploadFile(sourcePath, sourceFileName, targetPath, targetFileName);
                     return true;
                 }
@@ -138,13 +186,18 @@ public class ProcessBootStrap {
 
         /**
          * 执行单个命令
+         *
          * @param command command
          * @return
          */
-        public Operation command(String command){
+        public Operation command(String command) {
             AbstractProcess process = new AbstractProcess() {
                 @Override
                 public boolean invoke() throws Exception {
+                    if (shellThreadPool == null) {
+                        log.info("请配置ssh地址");
+                        return false;
+                    }
                     shellThreadPool.command(command);
                     return true;
                 }
@@ -155,13 +208,18 @@ public class ProcessBootStrap {
 
         /**
          * 执行多个命令
+         *
          * @param commands commands
          * @return
          */
-        public Operation commands(String... commands){
+        public Operation commands(String... commands) {
             AbstractProcess process = new AbstractProcess() {
                 @Override
                 public boolean invoke() throws Exception {
+                    if (shellThreadPool == null) {
+                        log.info("请配置ssh地址");
+                        return false;
+                    }
                     shellThreadPool.commands(commands);
                     return true;
                 }
@@ -172,9 +230,10 @@ public class ProcessBootStrap {
 
         /**
          * 内置执行
+         *
          * @return
          */
-        public ProcessBootStrap execute(){
+        public ProcessBootStrap execute() {
             processBootStrap.execute();
             return processBootStrap;
         }
--- src/main/java/org/github/process/ProcessBootStrapApplication.java
+++ src/main/java/org/github/process/ProcessBootStrapApplication.java
@@ -1,11 +1,22 @@
 package org.github.process;
 
+import lombok.SneakyThrows;
 import org.github.process.choice.FTPOperation;
-import org.github.process.config.Configuration;
+import org.github.process.thread.FileCompleteListener;
 import org.github.process.tool.impl.FtpProcess;
 import org.github.process.tool.impl.ShellProcess;
 import org.github.process.util.ShellThreadPool;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.text.DecimalFormat;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author : zhilin
@@ -13,14 +24,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class ProcessBootStrapApplication {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
 //        SpringApplication.run(ProcessBootStrapApplication.class);
 
         new ProcessBootStrap().stream()
-                .upLoadFile("D:\\","test.jar","/home","lzl.jar")
-                .command("nohup java -jar /home/lzl.jar > /home/a.log 2>&1 &")
+                .upLoadFile("D:\\", "test.jar", "/home", "test.jar")
+                .command("nohup java -jar /home/test.jar > /home/a.log 2>&1 &")
                 .execute()
                 .shutdown();
+
+
 
     }
 }
